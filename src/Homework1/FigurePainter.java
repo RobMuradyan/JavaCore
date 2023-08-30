40a51b600ea708e8abccfe025c9252339dbcd62d
RobMuradyan <robmuradyan777@gmail.com>
2023-08-30 12:26:25 +0400

    h

--- src/Homework1/FigurePainter.java
+++ src/Homework1/FigurePainter.java
@@ -3,9 +3,9 @@ package Homework1;
 public class FigurePainter {
     public static void main(String[] args) {
         //Patker 1
-        int count=1;
-        for (int i =0; i <5 ; i++){
-            for (int j = 0; j <count ; j++){
+        int count = 1;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < count; j++) {
                 System.out.print("* ");
             }
             System.out.println();
@@ -16,7 +16,7 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
         //patker2
-        count=5;
+        count = 5;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < count; j++) {
                 System.out.print("* ");
@@ -32,7 +32,7 @@ public class FigurePainter {
         int num = 5;
 
         for (int i = 1; i <= num; i++) {
-            for (int j = 1; j <=num - i; j++) {
+            for (int j = 1; j <= num - i; j++) {
                 System.out.print("  ");
             }
 
@@ -45,18 +45,79 @@ public class FigurePainter {
         System.out.println();
         System.out.println();
         //patker4
-        for (int i = 1; i <=num ; i++) {
-            for (int j = 1; j <i ; j++) {
-                System.out.print("  ");}
-            for (int k = i; k <=num ; k++) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = i; k <= num; k++) {
                 System.out.print("* ");
             }
             System.out.println();
 
-            }
+        }
+        System.out.println();
+        System.out.println();
 
+        //patker5
+
+
+        int count1 = 1;
+        int count3 = 4;
+        for (int i = 0; i < 5; i++) {
+            for (int k = 1; k <= count3; k++) {
+                System.out.print(" ");
+
+            }
+            {
+                for (int j = 0; j < count1; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+                count1++;
+                count3--;
+            }
+        }
+        int count4 = 5;
+        int count2 = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print(" ");
+
+            for (int k = 0; k < count2; k++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j < count4; j++) {
+                System.out.print("* ");
+
+
+            }
+            System.out.println();
+            count4--;
+            count2++;
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
